b806c66f3584fc5bac0f9f8aabdeb4f6f12b484f
ramazan_kashipov <ramazan.kashipov@systeme.ru>
2023-11-06 16:30:26 +0300

    Redirect to manager/orders after change and save if went to order admin from manager/orders previously

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.shortcuts import reverse
+from django.shortcuts import reverse, redirect
 from django.templatetags.static import static
 from django.utils.html import format_html
-
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.conf import settings
 from .models import Order, OrderItem
 from .models import Product
 from .models import ProductCategory
@@ -20,6 +21,14 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         OrderItemInline,
     ]
+
+    def response_change(self, request, obj):
+        res = super(OrderAdmin, self).response_change(request, obj)
+        if 'next' in request.GET:
+            if url_has_allowed_host_and_scheme(request.GET['next'], settings.ALLOWED_HOSTS):
+                return redirect(request.GET['next'])
+        else:
+            return res
 
     def save_formset(self, request, form, formset, change):
         instances = formset.save(commit=False)
